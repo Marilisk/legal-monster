3d33ce2181a7b575ab82ad356396dc87ef2fb952
Marilisk <6868221@gmail.com>
2023-12-03 11:07:16 +0300

    mui & activity types

--- src/Components/Clients/EditClient/EditClientForm/NotesArray/LastActionItem/ActionItem.tsx
+++ src/Components/Clients/EditClient/EditClientForm/NotesArray/LastActionItem/ActionItem.tsx
@@ -1,21 +1,23 @@
 import { FC } from "react"
-import { INote } from "../../../../../../types/clientsTypes"
+import { IActivity } from "../../../../../../types/clientsTypes"
 import c from './ActionItem.module.scss'
 import { DoneTask } from "../../../../../../assets/Icons/DoneTask"
 import { NoBorderButton } from "../../../../../../assets/input elements/NoBorderButton/NoBorderButton"
 import { brieflyformatDate } from "../../../../../../assets/functions/formatDate"
 import { DeleteButton } from "../../../../../../assets/input elements/DeleteButton/DeleteButton"
+import { Card, CardContent, Paper } from "@mui/material"
 
 
 interface IActionProps {
-    note: INote
-    edit: (arg: INote) => void
+    note: IActivity
+    edit: (arg: IActivity) => void
     deleteItem: () => void
     hoveredLine: string
     setHoveredLine: (arg: string) => void
+    setEditableNote?: (arg: IActivity) => void
 }
 
-export const ActionsItem: FC<IActionProps> = ({ note, edit, deleteItem, hoveredLine, setHoveredLine }: IActionProps) => {
+export const ActionsItem: FC<IActionProps> = ({ note, edit, deleteItem, hoveredLine, setHoveredLine, setEditableNote }: IActionProps) => {
 
 
 
@@ -24,21 +26,35 @@ export const ActionsItem: FC<IActionProps> = ({ note, edit, deleteItem, hoveredL
         onMouseLeave={() => setHoveredLine('')}
     >
         <div className={c.col}>{note.author.fullName}</div>
-        <div className={c.titleCol}>{note.title}</div>
+        <div className={c.titleCol}>
+            <div>{note.title}</div>
+            <div>{note.text}</div>
+        </div>
+        <Card>
+            <CardContent>
+            Результат:
+            {note.result}
+
+            </CardContent>
+        </Card>
+
         <div className={c.col}>{brieflyformatDate(new Date(note.deadLine))}</div>
         <div className={c.doneCol}>
 
             {
                 !note.isDone ?
-                    <NoBorderButton type="button" small
-                        callBack={() => edit({ ...note, isDone: true })} >
-                        <div>отметить выполненным</div>
-                    </NoBorderButton>
+                    <>
+                        <NoBorderButton type="button" small
+                            callBack={() => edit({ ...note, isDone: true })} >
+                            <div>отметить выполненным</div>
+                        </NoBorderButton>
+                    </>
                     :
                     <DoneTask />
             }
 
             <DeleteButton callBack={deleteItem} visible={hoveredLine === note._id} />
+            {setEditableNote && <button onClick={() => setEditableNote(note)} >редактировать</button> }
 
         </div>
     </div>
